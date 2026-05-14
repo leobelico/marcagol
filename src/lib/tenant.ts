0760f065019410495c8d36eef340f212b2ff6388
Sistemas <ldj821@gmail.com>
2026-05-14 09:40:34 -0600

    idk4

--- src/lib/tenant.ts
+++ src/lib/tenant.ts
@@ -14,9 +14,13 @@ export async function getTenant() {
 
     const isLocalhost = parts.length === 2 && parts[1] === "localhost";
     const isLvh = parts.length === 3 && parts[1] === "lvh" && parts[2] === "me";
+    const appDomain = process.env.NEXT_PUBLIC_APP_DOMAIN?.replace(/:\d+$/, "") || "";
+    const isProduction = appDomain && host.endsWith(`.${appDomain}`) && host !== appDomain;
 
     if (isLocalhost || isLvh) {
       slug = parts[0];
+    } else if (isProduction) {
+      slug = host.replace(`.${appDomain}`, "");
     }
   }
 
